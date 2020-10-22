577221f13466130e120cfdf80fa9cb202881e089
github_address <s.lamichhane.228@gmail.com>
2020-10-22 12:53:04 -0400

    save

---
+++ main.js
@@ -0,0 +1,25 @@
+"use strict";
+exports.__esModule = true;
+var message = 'welcome back';
+console.log(message);
+var isBegginer = true;
+var total = 10;
+var name = "sabina";
+console.log(total);
+var sentance = "My name is " + name + "\nI am a begginer in Typescript";
+console.log(sentance);
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 5];
+var list3 = [20, "sabina"];
+var color;
+(function (color) {
+    color[color["Red"] = 0] = "Red";
+    color[color["Green"] = 1] = "Green";
+    color[color["Blue"] = 2] = "Blue";
+})(color || (color = {}));
+;
+var c = color.Green;
+console.log(c);
+var myVariable = 10;
+console.log(myVariable.name);
+myVariable();
